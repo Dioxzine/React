e5dc65e9b7388d7979d87f99176507746588908c
jiahong zhou <jiahong.zhou@marconirovereto.it>
2023-08-29 15:28:42 +0200

    comp TPST

---
+++ community-connect/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className = "background">
+      <div className='titolo'>
+        <h1>
+          Community Connect
+        </h1>
+      </div>
+      <div>
+        <nav className= "BarraDiRicerca">
+
+        </nav>
+        <button className='PulsanteDiRicerca'> 
+        </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
